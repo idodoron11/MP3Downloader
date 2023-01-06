041f3fc2fb31a02a932681b2ca82bf86b938eddc
ihirsche <ihirsche@cisco.com>
2023-01-06 23:17:59 +0200

    Improve download monitoring Improve file naming Improve wait times

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import traceback
 import selenium.common.exceptions
 from selenium import webdriver
@@ -86,15 +88,15 @@ class Downloader:
         })
         self.browser = webdriver.Chrome(options=options)
 
-    def open_download_page(self, track_id):
+    def open_download_page(self, track):
         logging.info("Going back to homepage")
         self.browser.get("https://free-mp3-download.net/")
         try:
             WebDriverWait(self.browser, 30).until(
                 EC.presence_of_element_located((By.XPATH, '//button[@id="snd"]'))
             )
-            logging.info(f"Opening the download page of track {track_id}")
-            self.browser.execute_script(f'window.location.href = "https://free-mp3-download.net/download.php?id={track_id}"')
+            logging.info(f"Opening the download page of track {track.id}")
+            self.browser.execute_script(f'window.location.href = "https://free-mp3-download.net/download.php?id={track.id}"')
             logging.debug("Waiting for page load")
             WebDriverWait(self.browser, 30).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "dl"))
@@ -130,36 +132,54 @@ class Downloader:
         download_btn = self.browser.find_element(By.CLASS_NAME, "dl")
         self.wait_engine.wait()
         download_btn.click()
-        self.wait_engine.wait(MINIMUM_WAIT_AFTER_DOWNLOAD)
 
-    def download(self, track_id):
+
+    def wait_for_download_finish(self, success_cb=lambda *args: None, failure_cb=lambda *args: None, wait_time=1):
+        logging.info("Waiting for download completion")
+        wait_until = datetime.datetime.now() + datetime.timedelta(minutes=wait_time)
+        while datetime.datetime.now() <= wait_until:
+            dir_content = glob.glob(os.path.join(self.download_path, "*.*"))
+            if len(dir_content) == 0:
+                logging.debug("Download has not started yet")
+            else:
+                latest_file = max(dir_content, key=os.path.getctime)
+                _, extension = os.path.splitext(latest_file)
+                if extension == ".crdownload":
+                    logging.debug("Download has not finished yet")
+                else:
+                    return success_cb(latest_file)
+            time.sleep(1)
+        return failure_cb()
+
+    def download(self, track):
+        def on_download_success(filepath, target_dir=None):
+            artist = track.artist.name
+            album = track.album.title
+            if target_dir is None:
+                target_dir = os.path.join(self.download_path, artist, album)
+            if not os.path.exists(target_dir):
+                os.makedirs(target_dir)
+            new_filepath = os.path.join(target_dir, f"{track.disk_number}-{track.track_position:02} {track.artist.name} - {track.title}.{self.format}")
+            shutil.move(filepath, new_filepath)
+            logging.info(f"Track {track.id} has been saved to {new_filepath}")
+
+        def on_download_failure():
+            raise Exception("Download failure")
+
         self.wait_engine.resume()
-        self.open_download_page(track_id)
+        self.open_download_page(track)
         try:
             self.process_download_page()
-        except selenium.common.exceptions.NoSuchElementException:
-            logging.error(f"Could not download {track_id}.")
+            self.wait_for_download_finish(success_cb=on_download_success, failure_cb=on_download_failure)
+        except (selenium.common.exceptions.NoSuchElementException, Exception) as e:
+            logging.error(f"Could not download {track.artist.name} - {track.title}.")
+            logging.error(traceback.format_exc())
         finally:
             self.wait_engine.pause()
 
-    def download_list(self, list):
-        for index, item in enumerate(list):
-            self.download(item)
-
-    def tidy_up_downloaded_files(self, subfolder1, subfolder2):
-        target_dir = os.path.join(self.download_path, subfolder1)
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
-        target_dir = os.path.join(target_dir, subfolder2)
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
-        files = glob.glob(os.path.join(self.download_path, "*.*"))
-        files = [(os.path.basename(filepath), filepath) for filepath in files]
-        files.sort(key=lambda item: os.path.getmtime(item[1]))
-        for index, (filename, filepath) in enumerate(files):
-            new_filepath = os.path.join(self.download_path, f"{index + 1:02} {filename}")
-            shutil.move(filepath, new_filepath)
-            shutil.move(new_filepath, target_dir)
+    def download_tracks(self, track_list):
+        for index, track in enumerate(track_list):
+            self.download(track)
 
 
 def process_deezer_url(url):
@@ -173,30 +193,30 @@ def process_deezer_url(url):
         album_id = urlId
         album = client.get_album(album_id)
         artist = album.get_artist()
-        return artist.name, album.title, [track.id for track in
-                                          client.get_album(album_id).get_tracks()]
+        return artist.name, album.title, client.get_album(album_id).get_tracks()
     elif urlType == "track":
         track_id = urlId
         track = client.get_track(track_id)
         artist = track.get_artist()
         album = track.get_album()
-        return artist.name, album.title, [track_id]
+        return artist.name, album.title, track
     elif urlType == "playlist":
         playlist_id = urlId
         playlist = client.get_playlist(playlist_id)
-        return "Playlists", playlist.title, [track.id for track in
-                                             playlist.get_tracks()]
+        return "Playlists", playlist.title, playlist.get_tracks()
 
 
 def main():
-    user_format = input("Choose a format (mp3 / flac): ")
+    is_interactive = len(sys.argv) != 3
+    user_format = input("Choose a format (mp3 / flac): ") if is_interactive else sys.argv[1]
     downloader = Downloader(user_format)
 
     while 1:
-        deezer_url = input("Enter a deezer url: ")
-        artist, album, song_ids = process_deezer_url(deezer_url)
-        downloader.download_list(song_ids)
-        downloader.tidy_up_downloaded_files(artist, album)
+        deezer_url = input("Enter a deezer url: ") if is_interactive else sys.argv[2]
+        artist, album, tracks = process_deezer_url(deezer_url)
+        downloader.download_tracks(tracks)
+        if not is_interactive:
+            break
         stay_in_loop = input("Would you like to download more stuff? (yes / no): ")
         if stay_in_loop.lower() == "no":
             break
